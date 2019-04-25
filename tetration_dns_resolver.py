0ec540243cc73af6ce4a2049eb0eb78160de04c3
Brandon Beck <beckbran@gmail.com>
2019-04-25 12:06:54 -0500

    updated to only include hostname annotations

--- tetration_dns_resolver.py
+++ tetration_dns_resolver.py
@@ -62,7 +62,7 @@ requests.packages.urllib3.disable_warnings()
 
 TETRATION_API_URL = "https://<tetration IP or Hostname>"
 TETRATION_API_CRED_PATH = '<tetration credential file>.json'
-TETRATION_HOST_NAME_USER_ANNOTATION = 'Hostname'
+TETRATION_HOST_NAME_USER_ANNOTATION = 'FQDN'
 TETRATION_SCOPE_NAME = 'Default'
 TETRATION_SEARCH_LIMIT = 20
 DNS_SERVERS = [] # Example ['8.8.8.8','8.8.1.1']
@@ -142,8 +142,10 @@ def ResolveUnnamedHosts(inventoryList):
         try:
             addr = dns.reversename.from_address(host["ip"])
             host_name = str(resolver.query(addr,"PTR")[0])
-            host.update({"user_" + TETRATION_HOST_NAME_USER_ANNOTATION: host_name[:-1] })
-            resolved_hosts.append(host)
+            resolved_hosts.append({
+                'IP': host['ip'],
+                TETRATION_HOST_NAME_USER_ANNOTATION: host_name[:-1]
+            })
         except:
             print("Couldn't resolve IP: {ip}".format(ip=host["ip"]))
             continue
@@ -155,28 +157,14 @@ Create annotation csv and push to Tetration
 ------------------------------------------------------------------------------------
 '''
 def SendAnnotationUpdates(rc,resolved_hosts):
-    user_annotations = []
-    headerFlag = 0
-
-    for host in resolved_hosts:
-        row = dict([(k if not k.startswith('user_') else k.split('user_')[1],v) for k,v in host.items() if k.startswith(('ip', 'vrf_name', 'user_'))])
-        row['IP'] = row.pop('ip')
-        row['VRF'] = row.pop('vrf_name')
-        user_annotations.append(row)
-        if headerFlag == 0:
-            headers = [key for key in row if key != 'IP' and key != 'VRF']
-            headers.insert(0,'VRF')
-            headers.insert(0,'IP')
     with open('annotations.csv', 'wb') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=headers)
+        writer = csv.DictWriter(csvfile, fieldnames=['IP', TETRATION_HOST_NAME_USER_ANNOTATION])
         writer.writeheader()
-        writer.writerows(user_annotations)
+        writer.writerows(resolved_hosts)
 
     file_path = 'annotations.csv'
-    keys = ['IP', 'VRF']
-
-    req_payload = [tetpyclient.MultiPartOption(key='X-Tetration-Key', val=keys), tetpyclient.MultiPartOption(key='X-Tetration-Oper', val='add')]
-    resp = rc.upload(file_path, '/assets/cmdb/upload', req_payload)
+    req_payload = [tetpyclient.MultiPartOption(key='X-Tetration-Oper', val='add')]
+    resp = rc.upload(file_path, '/assets/cmdb/upload/%s' % TETRATION_SCOPE_NAME, req_payload)
     if resp.status_code != 200:
         print("Error posting annotations to Tetration cluster")
     else:
